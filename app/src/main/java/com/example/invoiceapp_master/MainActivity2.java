9b928ea0f4d53f33ae86f692bef4a7f3ff3ecc07
Khushank Mistry <47456301+Khushank1120@users.noreply.github.com>
2022-10-05 23:30:22 +0530

    Update MainActivity2.java

--- app/src/main/java/com/example/invoiceapp_master/MainActivity2.java
+++ app/src/main/java/com/example/invoiceapp_master/MainActivity2.java
@@ -9,6 +9,37 @@ public class MainActivity2 extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+          
+         
+    private void createPDF() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
 
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this,"PDF Created in - My Files/Internal Storage", Toast.LENGTH_LONG).show();
+
+                PdfDocument myPdfDocument = new PdfDocument();
+                Paint myPaint = new Paint();
+
+                PdfDocument.PageInfo myPageInfo1 = new PdfDocument.PageInfo.Builder(2480, 3508, 1).create();
+                PdfDocument.Page myPage1 = myPdfDocument.startPage(myPageInfo1);
+
+                Canvas canvas = myPage1.getCanvas();
+                canvas.drawBitmap(scaledBitmap,10,10,myPaint);
+
+                canvas.drawBitmap(scaledBitmap2,1700,3010,myPaint);    myPdfDocument.finishPage(myPage1);
+
+                File file = new File(Environment.getExternalStorageDirectory(), "/1PerformaVisionArt.pdf");
+                try {
+                    myPdfDocument.writeTo(new FileOutputStream(file));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myPdfDocument.close();
+
+            }
+        });
     }
+  }
 }
